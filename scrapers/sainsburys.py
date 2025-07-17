e6b16a83a7670452059917a1c09e29f740c9eb92
Scoutch007 <129302954+Scoutch007@users.noreply.github.com>
2025-07-17 13:29:07 +0100

    Create sainsburys.py

---
+++ scrapers/sainsburys.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+def search_sainsburys(query):
+    url = f"https://www.sainsburys.co.uk/gol-ui/SearchResults/{query.replace(' ', '%20')}"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    res = requests.get(url, headers=headers)
+
+    if res.status_code != 200:
+        return []
+
+    soup = BeautifulSoup(res.text, "html.parser")
+    products = []
+
+    for item in soup.select(".pt-grid-item"):
+        try:
+            name = item.select_one(".pt__link").text.strip()
+            price_text = item.select_one(".pt__price").text.strip()
+            price = float(price_text.replace("£", "").split()[0])
+            link = "https://www.sainsburys.co.uk" + item.select_one("a")["href"]
+            image = item.select_one("img")["src"]
+            products.append({
+                "supermarket": "Sainsbury’s",
+                "name": name,
+                "price": price,
+                "url": link,
+                "image": image
+            })
+        except:
+            continue
+
+    return products
